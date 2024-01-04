cc6852355db29e6e403977a7000a580fcfbbb3da
Herr Gromov <youriy.gromov@gmail.com>
2024-01-04 18:07:04 +0100

    Add new file scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@ alert('Hello world');
 
 let favoriteFood = 'pizza';
 document.write(favoriteFood);
+
+let escapedText2 = 'He said: "Yes!", that\'s for sure.';
+console.log(escapedText2); 
+
+let escapedText = 'He said: "Yes!", that\'s for sure.';
