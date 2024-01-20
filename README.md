69ea9e50643437066cf09bf33a0e0248cb6a997a
Herr Gromov <youriy.gromov@gmail.com>
2024-01-20 19:14:14 +0100

    Implemented a modal as a details view for each Pokémon in list

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Pokédex App
-A small web application built with HTML, CSS and JavaScript that loads data from an external API and enables the viewing of data points in detail.
+# JavaScript Application - Pokémon Encyclopedia
+
+## Project Overview
+A compact web application built with HTML, CSS, and JavaScript, fetching data from an external API for an interactive experience in viewing detailed data points.
+
+## Core Functionalities
+* Integration with an external API for data retrieval.
+* Initial loading of a specific data set, with a functionality for users to request additional data through a 'Load More' button.
+* Presentation of a list showcasing Pokémon names.
+* Feature to load and display detailed information about a selected Pokémon in a pop-up modal.
+* Capability for users to perform searches on the website for specific Pokémon names.
+
+## API Source for the Project
+The application fetches data from pokeapi.co.
+
+## Utilized Libraries
+The app incorporates Bootstrap for styling and layout, and jQuery for simplified DOM manipulation and AJAX requests.
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,88 @@
 'use strict';
 
 const pokemonRepository = (function () {
+
+  function showModal(title, text, imgurl, types) {
+    console.log(imgurl);
+    let modalContainer = document.querySelector('#modal-container');
+  
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+  
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+  
+    // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+  
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = 'Height: ' + text;
+         
+    let pokemonTypes = document.createElement('p');
+    pokemonTypes.innerText = 'Types: ';
+
+    types.forEach(function (current, index) {
+        // Create a span for each type
+        let newType = document.createElement('span');
+        newType.innerText = current.type.name;
+        
+        // Append the type span to the p element
+        pokemonTypes.appendChild(newType);
+
+        // Add a comma and a space after each type except the last
+        if (index < types.length - 1) {
+            pokemonTypes.appendChild(document.createTextNode(', '));
+        }
+    });
+
+    let imageElement = document.createElement('img');
+    imageElement.setAttribute('src', imgurl);
+    imageElement.setAttribute('width', '50%');
+    imageElement.setAttribute('height', 'auto');
+    imageElement.setAttribute('alt', 'Pokemon image');
+  
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(imageElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(pokemonTypes);
+    modalContainer.appendChild(modal);
+    
+    modalContainer.classList.add('is-visible');
+
+    modalContainer.addEventListener('click', (e) => {
+      // Since this is also triggered when clicking INSIDE the modal
+      // We only want to close if the user clicks directly on the overlay
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+  }
+
+  // Close modal
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+  });
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content!');
+  });
+
   const pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
@@ -75,6 +157,7 @@ const pokemonRepository = (function () {
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
       console.log(pokemon);
+      showModal(pokemon.name, pokemon.height, pokemon.imageUrl, pokemon.types);
     });
   }
 
